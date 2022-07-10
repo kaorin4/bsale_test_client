3126464ff25604d2c496d8bccf88d750daff6d01
kaorin4 <kaori.murakami4@gmail.com>
2022-07-09 21:11:24 -0500

    added category filter

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,29 @@
 import displayProductCard from './components/productCard.js';
 
-const getProductsData = async () => {
+const getCategoriesData = async () => {
+
+  return fetch('http://localhost:5000/api/categories')
+    .then(res => {
+      if(!res.ok){
+        throw Error('error');
+      }
+      return res.json();
+    })
+    .then(data => {
+      addCategories(data.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const getProductsData = async (url) => {
+
+  document.querySelector('#products-list').innerHTML = '';
 
   try {
 
-    const response = await fetch("http://localhost:5000/api/products");
+    const response = await fetch(url);
 
     if(!response.ok){
       throw new Error(response.status);
@@ -24,5 +43,50 @@ const getProductsData = async () => {
     
 }
 
+const filterProductsByCategory = (e) => {
 
-getProductsData();
+  e.preventDefault();
+
+  const selectFilter = document.querySelector('#categories-filter');
+  const selectedCategory = selectFilter.options[selectFilter.selectedIndex].value;
+
+  const url = selectedCategory ? `http://localhost:5000/api/products/category/${selectedCategory}` : `http://localhost:5000/api/products`;
+
+  document.querySelector('#search-input').value = "";
+  getProductsData(url);
+}
+
+const addCategories = (categories) => {
+
+  const selectFilter = document.querySelector('#categories-filter');
+
+  categories.map(category => {
+
+    const option = new Option(category.name, category.id);
+    selectFilter.appendChild(option);
+  });
+
+}
+
+const addListeners = () => {
+
+  // select
+  const selectFilter = document.querySelector('#categories-filter');
+  selectFilter.addEventListener('change', filterProductsByCategory);
+}
+
+
+const load = async () => {
+
+  // load categories options
+  await getCategoriesData();
+  // add event listeners
+  addListeners();
+
+  let url = "http://localhost:5000/api/products";
+  getProductsData(url);
+
+}
+
+load();
+
